99f29a1afdd49b2c023e856b97ceb41b0bd7b0c6
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2021-12-02 12:36:05 -0600

    Add test for framework snippet

---
+++ WPFAquariumSln/WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAquariumObjects
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Test();
+        }
+
+        public static void Test()
+        {
+            Aquarium aquarium = new Aquarium();
+            Aquarium.SetIsBubbleSource(aquarium, true);
+            bool isBubbleSource = Aquarium.GetIsBubbleSource(aquarium);
+            Debug.WriteLine($"Is bubble source: {isBubbleSource}");
+        }
+
+        public class Aquarium : AquariumObject
+        {
+            protected override Geometry DefiningGeometry => throw new NotImplementedException();
+        }
+    }
+}
